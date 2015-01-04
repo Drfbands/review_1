43c2b33317ad665573ba7d0af4245d94352f5400
Brian Jones <drf0124@yahoo.com>
2015-01-03 20:54:51 -0500

    added the page for the store

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,8 @@ class PagesController < ApplicationController
 
   def used
   end
+
+  def store
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get "about" => "pages#about"
   get "app" => "pages#app"
+  get "store" => "pages#store"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
